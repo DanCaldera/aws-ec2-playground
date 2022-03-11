1c4bedc590ab7bf34e1d7df233222cc5562560ea
EC2 Default User <ec2-user@ip-172-31-15-89.us-west-1.compute.internal>
2022-03-11 11:06:49 +0000

    updated folders

--- dev/node-app/app.js
+++ dev/node-app/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hola mundooasoo!</h1>')
+    res.send('<h1>Hola Mundooo!</h1>')
 })
 
 app.listen(5000, () => {

336acb14484771ec77f69a50ddc2880f49b6c073
EC2 Default User <ec2-user@ip-172-31-15-89.us-west-1.compute.internal>
2022-03-11 10:49:56 +0000

    updated

--- dev/node-app/app.js
+++ dev/node-app/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hola mundooasdasdoo!</h1>')
+    res.send('<h1>Hola mundooasoo!</h1>')
 })
 
 app.listen(5000, () => {

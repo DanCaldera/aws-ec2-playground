5f94aeda31e845ce2b2b3e4c9a2a931c3251125b
EC2 Default User <ec2-user@ip-172-31-15-89.us-west-1.compute.internal>
2022-03-12 16:23:55 +0000

    updated some things in flaks app

--- dev/flask-app/app.py
+++ dev/flask-app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_flask():
     return '<h1>Hola desde Python!</h1>'
-    
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='13.56.185.213', port=5001)

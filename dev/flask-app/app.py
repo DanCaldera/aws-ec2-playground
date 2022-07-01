d4fd9727fb5e22b5b7add4951929c321897a633f
Daniel Caldera <dan@wait3.com>
2022-06-30 20:44:03 -0500

    basic upload audio example

--- dev/flask-app/app.py
+++ dev/flask-app/app.py
@@ -1,16 +1,48 @@
-from flask import Flask
-from 
+import os
+from flask import Flask, request, redirect
+from pydub import AudioSegment
+
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
 ALLOWED_EXTENSIONS = {'aac', 'mp3', 'ogg', 'midi', 'mpeg', 'wav'}
+UPLOAD_FOLDER = '/'
 
-@app.route('/')
-def hello():
-    return '<h1>Bienvenido al convertidor de audios de MisFans!</h1>'
 
-@app.route('/convert')
-def convert_audio():
-    return '<h1>Converter</h1>'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+            return redirect('/')
+
+        file = request.files['file']
+
+        if file.filename == '':
+            print('No selected file')
+            return redirect('/')
+        elif file and allowed_file(file.filename):
+            file.save(file.filename)
+            return redirect('/')
+        else:
+            print('File type not allowed')
+            return redirect('/')
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)

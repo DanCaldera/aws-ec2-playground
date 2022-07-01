ea6b138d61986acbcbf2c4d450a84d12a465c7bf
EC2 Default User <ec2-user@ip-172-31-34-49.us-east-2.compute.internal>
2022-07-01 00:56:32 +0000

    asd

--- dev/flask-app/app.py
+++ dev/flask-app/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from 
 app = Flask(__name__)
 
+ALLOWED_EXTENSIONS = {'aac', 'mp3', 'ogg', 'midi', 'mpeg', 'wav'}
 
 @app.route('/')
-def hello_flask():
-    return '<h1>Hola desde Python!</h1>'
+def hello():
+    return '<h1>Bienvenido al convertidor de audios de MisFans!</h1>'
 
+@app.route('/convert')
+def convert_audio():
+    return '<h1>Converter</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)

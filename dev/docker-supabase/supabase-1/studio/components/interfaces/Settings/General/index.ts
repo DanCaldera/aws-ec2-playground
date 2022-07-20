8a95cf2e58dfdd154f9f1c09c78f50d3021d29d3
Daniel <bdcaldera@gmail.com>
2022-07-20 06:12:13 +0000

    deleted supabase

--- dev/docker-supabase/supabase-1/studio/components/interfaces/Settings/General/index.ts
+++
@@ -1,6 +0,0 @@
-import PauseProjectButton from './PauseProjectButton'
-import RestartServerButton from './RestartServerButton'
-
-import DeleteProjectButton from './DeleteProjectButton'
-
-export { PauseProjectButton, RestartServerButton, DeleteProjectButton }
